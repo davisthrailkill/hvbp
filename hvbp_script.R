54e8b36c3aca1b4a177023d9ba85d2b4a2c03a77
davisthrailkill <davis.thrailkill@gmail.com>
2018-12-21 10:55:19 -0600

    first commit

---
+++ hvbp_script.R
@@ -0,0 +1,8 @@
+library(tidyverse)
+
+tps <- read.csv("data/hvbp_tps.csv")
+clincial_care <- read.csv("data/hvbp_clinical.csv")
+pat_experience <- read.csv("data/hvbp_patient_experience.csv")
+safety <- read.csv("data/hvbp_safety.csv")
+efficiency <- read.csv("data/hvbp_efficiency.csv")
+
